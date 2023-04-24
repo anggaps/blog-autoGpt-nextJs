f3660b404c21ca7730f2a5830e8489cadf1b4c1e
Angga ps <80311292+anggaps@users.noreply.github.com>
2023-04-25 01:12:38 +0700

    navbar

---
+++ app/Components/Navbar.tsx
@@ -0,0 +1,50 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
+import SocialLinks from './SocialLink';
+
+const Navbar = () => {
+  return (
+    <header className='mb-5'>
+      <nav
+        className='
+      flex 
+      justify-between 
+      items-center 
+      w-full
+      bg-wh-900
+      text-wh-100
+      px-10
+      py-4 
+      relative 
+      shadow-sm 
+      font-mono'
+      >
+        <div className='hidden sm:block '>social link</div>
+        <SocialLinks />
+        <div className='flex justify-between items-center gap-10'>
+          <Link href='/'>Home</Link>
+          <Link href='/'>Trend</Link>
+          <Link href='/'>about</Link>
+        </div>
+        <div>
+          <p>Sign in</p>
+        </div>
+      </nav>
+      <div className='flex justify-between gap-8 mt-5 mb-4 mx-10'>
+        <div className='basis-2/3 md:mt-3 '>
+          <h1 className='text-3xl font-bold md:text-5xl'>Blog ai App</h1>
+          <p className='text-sm md:text-base'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </p>
+        </div>
+        <div className='basis-full relative w-auto h-32 bg-wh-100'>
+          image here
+        </div>
+      </div>
+      <hr className='border-1 mx-10' />
+    </header>
+  );
+};
+
+export default Navbar;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import Navbar from './Components/Navbar';
 import './globals.css';
 import { Open_Sans } from 'next/font/google';
 
@@ -15,7 +16,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang='en'>
-      <body className={openSans.className}>{children}</body>
+      <body className={openSans.className}>
+        <Navbar />
+        {children}
+      </body>
     </html>
   );
 }
